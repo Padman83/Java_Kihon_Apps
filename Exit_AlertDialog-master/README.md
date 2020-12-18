9f49f2995302b52bd901abcd524cf56522bd240c
Padman Selvamanickam <endlessgalaxy15@gmail.com>
2020-12-18 09:03:41 +0800

    Added Exit_AlertDialog

---
+++ Exit_AlertDialog-master/README.md
@@ -0,0 +1,21 @@
+## Exit_AlertDialog
+
+![ezgif com-optimize](https://user-images.githubusercontent.com/45048950/95012264-de8c4700-0669-11eb-9588-d5985d757929.gif)
+
+### 🧰 System Requirements
+
+* Android Studio
+* 100 % Java
+
+### 🛠️ Installation 
+
+#### Open in Android Studio, Get from Version Control -> enter repository URL.
+
+```
+https://github.com/Padman83/Exit_AlertDialog.git
+```
+
+#### Or Use the Terminal.
+
+```
+git clone https://github.com/Padman83/Exit_AlertDialog.git
---
+++ Exit_AlertDialog-master/app/src/main/java/com/example/exit_alertdialog/MainActivity.java
@@ -0,0 +1,52 @@
+package com.example.exit_alertdialog;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.AlertDialogLayout;
+
+import android.app.AlertDialog;
+import android.content.DialogInterface;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu,menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        int id = item.getItemId();
+        if (id == R.id.exit){
+            AlertDialog.Builder builder = new AlertDialog.Builder(MainActivity.this);
+            builder.setMessage("Do You Want To Exit ?");
+            builder.setCancelable(true);
+
+            builder.setNegativeButton("YES", new DialogInterface.OnClickListener() {
+                @Override
+                public void onClick(DialogInterface dialogInterface, int i) {
+                    finish();
+                }
+            });
+            builder.setPositiveButton("NO", new DialogInterface.OnClickListener() {
+                @Override
+                public void onClick(DialogInterface dialogInterface, int i) {
+                    dialogInterface.cancel();
+                }
+            });
+            AlertDialog alertDialog = builder.create();
+            alertDialog.show();
+        }
+        return true;
+    }
+}
