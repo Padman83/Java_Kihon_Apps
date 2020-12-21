856f001680745951ee0f649c3e8eecfe90d25d75
Padman Selvamanickam <endlessgalaxy15@gmail.com>
2020-12-21 22:24:02 +0800

    Added Get_Latitude_Longitude

---
+++ Get_Latitude_Longitude-master/README.md
@@ -0,0 +1,21 @@
+## Get_Latitude_Longitude
+
+![ezgif com-optimize](https://user-images.githubusercontent.com/45048950/95470973-fb02e900-09b3-11eb-9a9d-7712e2349af0.gif)
+
+### 🧰 System Requirements
+
+* Android Studio
+* 100 % Java
+
+### 🛠️ Installation 
+
+#### Open in Android Studio, Get from Version Control -> enter repository URL.
+
+```
+https://github.com/Padman83/Get_Latitude_Longitude.git
+```
+
+#### Or Use the Terminal.
+
+```
+git clone https://github.com/Padman83/Get_Latitude_Longitude.git
---
+++ Get_Latitude_Longitude-master/app/src/main/java/com/example/get_latitude_longitude/MainActivity.java
@@ -0,0 +1,55 @@
+package com.example.get_latitude_longitude;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.os.Handler;
+import android.os.Message;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    // Initialize the variables.
+    EditText etPlace;
+    Button btSubmit;
+    TextView tvAddress;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        etPlace = findViewById(R.id.et_place);
+        btSubmit = findViewById(R.id.bt_submit);
+        tvAddress = findViewById(R.id.tv_address);
+
+        btSubmit.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String address = etPlace.getText().toString();
+                GeoLocation geoLocation = new GeoLocation();
+                geoLocation.getAddress(address, getApplicationContext(), new GeoHandler());
+            }
+        });
+    }
+
+    private class GeoHandler extends Handler {
+        @Override
+        public void handleMessage(@NonNull Message msg) {
+            String address;
+            switch (msg.what) {
+                case 1:
+                    Bundle bundle = msg.getData();
+                    address = bundle.getString("address");
+                    break;
+                default:
+                    address = null;
+            }
+            tvAddress.setText(address);
+        }
+    }
+}
